7bdbb85ba86189b4b5119c8c7cab70fab2755ae7
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-05-28 13:23:28 +0200

    bis S.210

--- Admin/Views/layouts/main.view.php
+++ Admin/Views/layouts/main.view.php
@@ -1,4 +1,27 @@
-<header>
-    <h1>Adminbereich: Image Gallery</h1>
-</header>
+<!DOCTYPE html>
+<html lang="de">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/pico.css">
+    <link rel="stylesheet" href="../../css/pico.classless.min.css">
+    <title>Template</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Adminbereich: Image Gallery</h1>
+    </header>
+
+    <main class="grid">
+        <?= $content ?? ''; ?>
+    </main>
+
+    <footer>
+        <p>|edvgraz|</p>
+    </footer>
+</body>
+
+</html>
